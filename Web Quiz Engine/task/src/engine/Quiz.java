0b37b0cc4b867232e13e611e9f6c68483f696c4f
Chrispin Kabuya <chrispin.kabuya@gmail.com>
2020-09-11 18:50:22 +0200

    Completed stage 2 web-based quiz engine

--- Web Quiz Engine/task/src/engine/Quiz.java
+++ Web Quiz Engine/task/src/engine/Quiz.java
@@ -1,18 +1,54 @@
 package engine;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.Arrays;
+@JsonIgnoreProperties(value = "answer", allowSetters  = true)
 public class Quiz {
+    int id;
     String title;
     String text;
-    String options[] =new String[4];
+    String[] options =new String[4];
+    int answer;
 
     public Quiz(String title, String text, String[] options) {
         this.title = title;
         this.text = text;
         this.options = options;
     }
-
+    public Quiz(String title, String text, String[] options,int answer,int id) {
+        this.title = title;
+        this.text = text;
+        this.options = options;
+        this.answer = answer;
+        this.id = id;
+    }
+    public Quiz(String title, String text, String[] options,int id) {
+        this.title = title;
+        this.text = text;
+        this.options = options;
+        this.id = id;
+    }
     public Quiz() {
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
+    }
+
     public String getText() {
         return text;
     }
@@ -35,5 +71,16 @@ public class Quiz {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Quiz{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", text='" + text + '\'' +
+                ", options=" + Arrays.toString(options) +
+                ", answer=" + answer +
+                '}';
     }
 }
--- Web Quiz Engine/task/src/engine/QuizController.java
+++ Web Quiz Engine/task/src/engine/QuizController.java
@@ -1,33 +1,74 @@
 package engine;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.*;
 
 @RestController
 public class QuizController {
-
+    LinkedHashMap<Integer, Quiz> quizDB = new LinkedHashMap<>();
     public QuizController() {
     }
 
-    @PostMapping(path = "/api/quiz")
-    public Feedback quizAnswer(@RequestBody String answer) {
-        Feedback feedback = new Feedback(false, "Wrong answer! Please, try again.");
+
+    @PostMapping(path = "/api/quizzes")
+    public Quiz createQuiz(@RequestBody Quiz answer) {
         if (answer != null) {
             //answer=2 split on =
-            String ans = answer.split("=")[1]; // not sure about this but learning
-            if (ans.equals("2")) {
-                feedback = new Feedback(true, "Congratulations, you're right");
+            if(quizDB.isEmpty()){
+                answer.setId(1);
+                quizDB.put(1, answer);
             }
-
+            else{
+                int newID = quizDB.size()+1;
+                answer.setId(newID);
+                quizDB.put(newID, answer);
+            }
+            System.out.println(answer);
         }
-        return feedback;
+        return new Quiz(answer.getTitle(), answer.getText(), answer.getOptions(), answer.getId());
     }
 
-    @GetMapping(path = "/api/quiz")
-    public Quiz getTQuiz() {
-        String[] opt = {"Robot", "Tea leaf", "Cup of coffee", "Bug"};
-        return new Quiz("The Java Logo", "What is depicted on the Java logo?", opt);
+    @GetMapping(path = "/api/quizzes/{id}")
+    public Quiz getQuiz(@PathVariable int id) {
+        if( quizDB.containsKey(id)){
+            Quiz q = quizDB.get(id);
+            return new Quiz(q.getTitle(),q.getText(),q.getOptions(),q.getId());
+        }
+        else{
+             throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Not found"
+            );
+        }
+
+    }
+    @GetMapping(path = "/api/quizzes")
+    public Collection<Quiz> getAllQuizzes() {
+        return quizDB.values();
+    }
+    @PostMapping(path = "/api/quizzes/{id}/solve")
+    public Feedback solveQuiz(@PathVariable int id, @RequestBody String answer) {
+         Feedback feedback;
+         System.out.println("The answer is "+answer);
+        if( quizDB.containsKey(id)){ //check if the question is available
+            Quiz q = quizDB.get(id);
+            String ans = answer.split("=")[1]; // not sure about this but learning
+            System.out.println(q);
+             if(q.getAnswer() == Integer.parseInt(ans)){
+                 feedback = new Feedback(true,"Congratulations, you're right!");
+             }
+             else{
+                 feedback = new Feedback(false,"Wrong answer! Please, try again.");
+             }
+            return feedback;
+        }
+        else{
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Not found"
+            );
+        }
+
     }
 }
